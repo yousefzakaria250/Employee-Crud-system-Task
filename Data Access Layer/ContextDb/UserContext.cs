5a5852e5ff0be0d2935310c51218243e97a88b5a
el2user15 <elu839793@gmail.com>
2024-01-02 23:03:00 -0800

    Create Database Models

---
+++ Data Access Layer/ContextDb/UserContext.cs
@@ -0,0 +1,28 @@
+﻿using Db_Builder.Models.User_Manager;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Access_Layer.ContextDb
+{
+    public class UserContext : IdentityDbContext<AppUser>
+    {
+        public UserContext()
+        {
+                    
+        }
+
+        public UserContext( DbContextOptions<UserContext> options) :base(options)
+        {
+                
+        }
+
+        public DbSet<DegreeState> DegreeStates { set;get; }
+
+        
+    }
+}
