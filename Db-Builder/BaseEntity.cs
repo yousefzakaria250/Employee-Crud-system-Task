e2fa26d52a989ba7399a3c3f5407a1bfedfac50f
el2user15 <elu839793@gmail.com>
2024-01-02 21:17:25 -0800

    Add Project Structure

---
+++ Db-Builder/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Db_Builder
+{
+    internal class BaseEntity
+    {
+    }
+}
