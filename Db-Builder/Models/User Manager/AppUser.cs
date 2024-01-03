5b9b718b3ce6b277249812915e2c8080cc362051
el2user15 <elu839793@gmail.com>
2024-01-02 23:04:02 -0800

    Create Database Models

--- Db-Builder/Models/User Manager/AppUser.cs
+++ Db-Builder/Models/User Manager/AppUser.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Db_Builder.Models.User_Manager
 {
-    internal class AppUser
+    [Table(name: "User", Schema = "Security")]
+    public class AppUser :IdentityUser
     {
+        public string ? Name { set; get;  }
+
+        public string ? Email { set; get;  }
+
+        public string  ? PhoneNumber { set; get; }
+
+        public string ? User_Image { set; get;  }
+        [ForeignKey("DegreeState")]
+        public int ? DegreeStateId { set; get;  }
+        public DegreeState DegreeState { set; get; }
+       
+
     }
 }
---
+++ Db-Builder/Models/User Manager/DegreeState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Db_Builder.Models.User_Manager
+{
+    [Table(name: "DegreeState", Schema = "Security")]
+
+    public class DegreeState :BaseEntity
+    {
+        public string State {  get; set; }
+    }
+}
