1a416e091c6b1de56b435e845355f3914cbf07a8
el2user15 <elu839793@gmail.com>
2024-01-05 20:40:18 -0800

    Add Employee Controller

---
+++ User Manager.API/Controllers/EmployeeController.cs
@@ -0,0 +1,55 @@
+﻿using CORE_Layer.Dtos;
+using CORE_Layer.Helper;
+using CORE_Layer.Services;
+using CORE_Layer.Specification.Employee_Specs;
+using Db_Builder.Models.User_Manager;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace User_Manager.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeService _employeeService;
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpPost("AddUser")]
+        public async Task<IActionResult> Add([FromBody] AddUserDto UserDto)
+        {
+            var result = await _employeeService.Add(UserDto);
+            return Ok(result);
+        }
+
+
+        [HttpGet("GetAllUser")]
+        public async Task<ActionResult<GetUserDto>> GetAll([FromQuery] EmployeeSpecParams spec)
+        {
+            var result = await _employeeService.GetAllWithSpecs(spec);
+            if (result == null)
+                return Ok(new Response<AppUser>(404, "No Users yet"));
+            return Ok(result);
+        }
+
+
+        [HttpPut("Update")]
+        public async Task<IActionResult> Update(UpdateUserDto userDto)
+        {
+            var res = await _employeeService.UpdateUser(userDto);
+            return Ok(res);
+        }
+
+        [HttpPatch("DeleteUser")]
+       
+        public async Task<ActionResult<AddUserDto>> DeleteUser(string id)
+        {
+            var result = await _employeeService.DeleteUser(id);
+            return Ok(result);
+
+        }
+    }
+}
