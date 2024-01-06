663b4140b19620c6309e6948f579c99ea81abfa0
el2user15 <elu839793@gmail.com>
2024-01-05 20:44:47 -0800

    Add LoginDto And RegisterDto

---
+++ CORE Layer/Dtos/RegisterDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CORE_Layer.Dtos
+{
+    public class RegisterDto
+    {
+        [Required]
+        public string Name { set; get; }
+        [Required]
+        public string Email { set; get; }
+        [Required]
+        public string UserRole { get; set; }
+        [Required]
+        public string Password { set; get; }
+        [Required]
+        public string ConfirmPassword { set; get; }
+    }
+}
