68dcae7e50a722935ce1eb27531977ae7702c5b4
el2user15 <elu839793@gmail.com>
2024-01-05 20:43:08 -0800

    Add AuthResponse

---
+++ CORE Layer/Helper/AuthResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CORE_Layer.Helper
+{
+    public class AuthResponse
+    {
+        public string Message { get; set; }
+        public bool IsAuthenticated { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public string Token { get; set; }
+        public DateTime ExpiresOn { get; set; }
+    }
+}
